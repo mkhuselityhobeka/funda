9f3f3beb8fa3bf96365e04e07ae05c62852de2f0
mkhululityhobeka <mkhuselityhobeka@gmail.com>
2021-01-23 08:02:01 +0200

    unblocking https

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(express.static('./dist/funda-registration-front-end'));
 
+app.use(function (req, res, next) {
+    res.setHeader(
+      'Content-Security-Policy-Report-Only',
+      "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css; frame-src 'self';"
+    );
+    next();
+  });
+
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/funda-registration-front-end/index.html'}),
 );
