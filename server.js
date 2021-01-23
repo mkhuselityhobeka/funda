9023dd13fcdb1c30b44cca50aebfc0532826212a
mkhululityhobeka <mkhuselityhobeka@gmail.com>
2021-01-23 09:09:46 +0200

    changed back root directory in server.js file

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('/home/papi/DEV-TOOLS/FundaRegistration-Front-End/dist/funda-registration-front-end'));
+app.use(express.static('./dist/funda-registration-front-end'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '/home/papi/DEV-TOOLS/FundaRegistration-Front-End/dist/funda-registration-front-end/'}),
+    res.sendFile('index.html', {root: 'dist/funda-registration-front-end/'}),
 );
 
 app.listen(process.env.PORT || 8080);
