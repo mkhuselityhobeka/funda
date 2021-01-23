8ecc85bfc37c4437a8735edef3674c837792902e
mkhululityhobeka <mkhuselityhobeka@gmail.com>
2021-01-23 08:37:59 +0200

    added meata head in index.html

--- server.js
+++ server.js
@@ -3,17 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./dist/funda-registration-front-end'));
-
-app.use(function (req, res, next) {
-    res.setHeader(
-      'Content-Security-Policy-Report-Only',
-      "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css; frame-src 'self';"
-    );
-    next();
-  });
-
 app.get('/*', (req, res) =>
-    res.sendFile('/home/papi/DEV-TOOLS/FundaRegistration-Front-End/src/app/home-page/home-page.component.html', {root: 'dist/funda-registration-front-end/index.html'}),
+    res.sendFile('index.html', {root: 'dist/funda-registration-front-end/index.html'}),
 );
 
 app.listen(process.env.PORT || 8080);
